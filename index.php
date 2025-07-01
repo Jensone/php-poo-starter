8458ce5c30e9b0bcb053e0d8609913c39db2d04d
Jensone <hello@jensone.com>
2025-07-01 12:59:03 +0200

    Ajoute une nouvelle barre de navigation et un pied de page responsive pour améliorer la structure des vues HTML de l'application

--- index.php
+++ index.php
@@ -3,34 +3,8 @@
 require_once 'Model/Livre.php'; // Charge la classe Livre
 require_once 'Model/Utilisateur.php'; // Charge la classe Utilisateur
 
-// Instanciation de la classe Livre
-$petitPrince = new Livre(); // Ceci est un objet issue de la classe Livre
+include 'Views/navbar.html.php'; // Inclut le navbar
 
-$petitPrince
-    ->setNom('Le Petit Prince') // Méthode du parent Produit
-    ->setPrix(8) // Méthode du parent Produit
-    ->setDescription('Le petit princes est un roman') // Méthode du parent Produit
-    ->setCategorie('Fantastique') // Méthode du parent Produit
-    ->setNbPage(128)
-    ->setAuteur('Antoine de St-Exupéry')
-    ->setEdition('Gallimard')
-    ->setIsbn('9783140464079')
-; // L'opération de compléter un objet se nomme hydratation
+echo '<h1>En cours de conception</h1>';
 
-//--------------------------------------------------
-
-// Instanciation de la classe Utilisateur
-$bob = new Utilisateur();
-
-$bob
-    ->setNom('Bob')
-    ->setEmail('bob@gmail.com')
-    ->setIdentifiant('tahitibob')
-    ->setDateInscription(new \DateTime('1988-01-01')) // Les dates sont des objets
-;
-
-echo '<h1>Le livre nommé ' . $petitPrince->getNom() . ' est entre les mains de ' . $bob->getNom() . '.</h1>';
-
-echo '<h1>Le livre coûte ' . $petitPrince->getPrixEuro() . '</h1>';
-
-var_dump($petitPrince);
+include 'Views/footer.html.php'; // Inclut le footer
