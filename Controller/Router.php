9a2d9ab632abe5439b3f68350cad3ab20b5b3bb3
Jensone <hello@jensone.com>
2025-07-02 12:52:01 +0200

    Corrige la gestion des requêtes dans Router et ajoute une page 404 personnalisée pour améliorer l'expérience utilisateur

--- Controller/Router.php
+++ Controller/Router.php
@@ -12,7 +12,7 @@ class Router
         $this->request = $_SERVER["REQUEST_METHOD"]; // Récupère la méthode HTTP
     }
 
-    public function handleRequest() 
+    public function handleRequest()
     {
         switch ($this->request) { // On vérifie la méthode HTTP
             case 'GET': // En cas de requête GET
@@ -29,8 +29,25 @@ class Router
 
     public function getRequest()
     {
-        $url = $_SERVER["REQUEST_URI"]; // Récupère l'URL
-        return $url;
+        $uri = 'Views/pages' . $_SERVER["REQUEST_URI"]; // ça donne par exmp : "Views/pages/accueil"
+
+        if($_SERVER["REQUEST_URI"] == '/') { // On vérifie si l'URI est "/"
+            return $uri . 'accueil.html.php'; // Si oui, on renvoie la page d'accueil
+        } else { // Sinon, on vérifie si le template existe
+            return $this->getPage($_SERVER["REQUEST_URI"]);
+        }
+    }
+
+    public function getPage($uri)
+    {
+        $path = 'Views/pages' . $uri . '.html.php'; // On crée le chemin du template
+
+        if (file_exists($path)) { // Si le template existe
+            return $path; // Inclut la page correspondante
+        } else {
+            http_response_code(404); // Retourne une erreur 404
+            return 'Views/pages/404.html.php'; // Inclut la page 404 si le template n'existe pas
+        }
     }
 }
 // Ne rien écrire après cette accolade
--- index.php
+++ index.php
@@ -2,10 +2,9 @@
 
 require_once 'Controller/Router.php'; // Charge le router
 
-include 'Views/header.html.php'; // Inclut le header
+include_once 'Views/header.html.php'; // Inclut le header
 
 $router = new Router();
-include_once 'Views/pages' . $router->handleRequest() . '.html.php'; // Inclut la page correspondante
+include_once $router->getRequest();
 
-
-include 'Views/footer.html.php'; // Inclut le footer
+include_once 'Views/footer.html.php'; // Inclut le footer
